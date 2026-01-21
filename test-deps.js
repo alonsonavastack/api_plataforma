7040d1d10e339eb806a204a085e75ff8057b04ac
Alonso Nava <alonso.nava086@gmail.com>
2026-01-21 15:48:15 -0600

    p v1.0

--- test-deps.js
+++
@@ -1,6 +0,0 @@
-try {
-    import('archiver').then(() => console.log('✅ Archiver loaded'));
-    import('adm-zip').then(() => console.log('✅ Adm-Zip loaded'));
-} catch (e) {
-    console.error('❌ Error loading modules:', e);
-}
