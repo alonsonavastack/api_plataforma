a51d5c4ede68d589de8fdffe8fec44fdaef51095
Alonso Nava <alonso.nava086@gmail.com>
2026-02-03 18:38:06 -0600

    subiend v1.4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
         name: 'devhubsharks-api',
         script: './index.js',
         interpreter: 'node',
-        node_args: '--env-file .env',
+        // node_args removed: we load .env via dotenv in code (Node <22 doesn't support --env-file)
+        // node_args: '--env-file .env',
         instances: 1,
         autorestart: true,
         watch: false,
