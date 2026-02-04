3cfc68a2cae7e57ef324660f13f2d2fad43fa2ed
Alonso Nava <alonso.nava086@gmail.com>
2026-02-03 19:23:51 -0600

    subiend v1.5

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [{
+        name: 'devhubsharks-api',
+        script: './index.js',
+        interpreter: 'node',
+        // node_args removed: we load .env via dotenv in code (Node <22 doesn't support --env-file)
+        // node_args: '--env-file .env',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '500M',
+        env: {
+            NODE_ENV: 'production'
+        },
+        error_file: './logs/error.log',
+        out_file: './logs/out.log',
+        time: true
+    }]
+};
